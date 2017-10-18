f15b746693056e74329734d4d823e39be7ed446f
Nikhil Vibhav <nikhil.vibhav@capgemini.com>
2017-10-18 18:18:16 +0530

    Update indentation

--- src/main/kotlin/com/capgemini/rmg/configs/KotlinBookstoreConfig.kt
+++ src/main/kotlin/com/capgemini/rmg/configs/KotlinBookstoreConfig.kt
@@ -22,52 +22,52 @@ import org.springframework.core.io.Resource
 @ComponentScan(basePackages = arrayOf("com.capgemini.rmg"))
 class KotlinBookstoreConfig @Autowired constructor(val camelContext: CamelContext)
 
-val LOGGER : Logger = LoggerFactory.getLogger(KotlinBookstoreConfig::class.java)
+val LOGGER: Logger = LoggerFactory.getLogger(KotlinBookstoreConfig::class.java)
 
-    @Bean
-    fun contextConfiguration(metricRegistry: MetricRegistry): CamelContextConfiguration {
-        return object: CamelContextConfiguration {
-            override fun beforeApplicationStart(context: CamelContext) {
-                val configLocations = arrayOf("file:config/camel.properties",
-                        "file:config/env.properties")
-                val propertiesComponent = PropertiesComponent()
-                propertiesComponent.setLocations(configLocations)
-                LOGGER.info("Added properties component to camel context.")
+@Bean
+fun contextConfiguration(metricRegistry: MetricRegistry): CamelContextConfiguration {
+    return object : CamelContextConfiguration {
+        override fun beforeApplicationStart(context: CamelContext) {
+            val configLocations = arrayOf("file:config/camel.properties",
+                    "file:config/env.properties")
+            val propertiesComponent = PropertiesComponent()
+            propertiesComponent.setLocations(configLocations)
+            LOGGER.info("Added properties component to camel context.")
 
-                if (context.isAllowUseOriginalMessage) {
-                    context.isAllowUseOriginalMessage = false
-                    LOGGER.info("Turned OFF AllowUseOriginalMessage.")
-                }
-
-                if (!context.isTracing) {
-                    context.isTracing = false
-                    LOGGER.info("Turned OFF tracing")
-                }
+            if (context.isAllowUseOriginalMessage) {
+                context.isAllowUseOriginalMessage = false
+                LOGGER.info("Turned OFF AllowUseOriginalMessage.")
             }
 
-            override fun afterApplicationStart(camelContext: CamelContext?) {
+            if (!context.isTracing) {
+                context.isTracing = false
+                LOGGER.info("Turned OFF tracing")
             }
         }
+
+        override fun afterApplicationStart(camelContext: CamelContext?) {
+        }
     }
+}
 
-    @Bean
-    @Primary
-    fun bridgePropertyPlaceholder(): ArchaiusBridgePropertyPlaceholderConfigurer {
-        val configurer =  ArchaiusBridgePropertyPlaceholderConfigurer()
+@Bean
+@Primary
+fun bridgePropertyPlaceholder(): ArchaiusBridgePropertyPlaceholderConfigurer {
+    val configurer = ArchaiusBridgePropertyPlaceholderConfigurer()
 
-        configurer.setIgnoreResourceNotFound(true)
-        configurer.setInitialDelayMillis(5000)
-        configurer.setDelayMillis(5000)
-        configurer.setIgnoreDeletesFromSource(true)
-        configurer.setAllowMultiplePlaceholders(true)
-        configurer.setLocations(*arrayOf<Resource>(
-                FileSystemResource("config/camel.properties"),
-                FileSystemResource("config/env.properties"),
-                FileSystemResource("config/hystrix.properties"),
-                FileSystemResource("config/metrics.properties"),
-                FileSystemResource("config/application.properties"),
-                FileSystemResource("config/postcode-mapping.properties")))
-        LOGGER.info("Camel Archaius Bridge Property Placeholder is configured successfully.")
+    configurer.setIgnoreResourceNotFound(true)
+    configurer.setInitialDelayMillis(5000)
+    configurer.setDelayMillis(5000)
+    configurer.setIgnoreDeletesFromSource(true)
+    configurer.setAllowMultiplePlaceholders(true)
+    configurer.setLocations(*arrayOf<Resource>(
+            FileSystemResource("config/camel.properties"),
+            FileSystemResource("config/env.properties"),
+            FileSystemResource("config/hystrix.properties"),
+            FileSystemResource("config/metrics.properties"),
+            FileSystemResource("config/application.properties"),
+            FileSystemResource("config/postcode-mapping.properties")))
+    LOGGER.info("Camel Archaius Bridge Property Placeholder is configured successfully.")
 
-        return configurer
-    }
+    return configurer
+}
